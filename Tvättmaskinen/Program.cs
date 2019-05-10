5c22937283a4e8281be4ee4c33d7f3557b51d897
Adam <Adam.kiniouar@hyhs.se>
2019-05-10 14:38:11 +0200

    Lagt till metod för att tvätta 172 och 162.

---
+++ Tvättmaskinen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+   
+
+namespace Tvättmaskinen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var reader = new Reader();
+
+            var file = @"C:\Users\Adam_\Desktop\MiP";
+            var fileSave = @"C:\Users\Adam_\Desktop\MiP\172\";
+
+            reader.Wash(file, fileSave);
+
+
+            Console.Read();
+
+        }
+    }
+}
